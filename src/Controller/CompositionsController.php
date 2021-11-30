28494fe41f400a346a9f2c149da0953148b47c67
Yann Quillard <y38.quillard@gmail.com>
2021-11-30 11:16:44 +0100

    📘Add composition to database

---
+++ src/Controller/CompositionsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\CompositionService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CompositionsController extends AbstractController
+{
+    /**
+     * @var CompositionService
+     */
+    private $compositionService;
+
+    public function __construct(CompositionService $compositionService) {
+        $this->compositionService = $compositionService;
+    }
+
+    /**
+     * @Route("/compo", name="compo")
+     */
+    public function compo(): Response
+    {
+        $riotChallenger = $this->compositionService->getChallengers();
+        $createComposition = $this->compositionService->createComposition($riotChallenger);
+
+        return $this->json($riotChallenger);
+    }
+}
