f4a1248d88e00c680ec9e272ce65bbc4e7b92de3
Yann Quillard <y38.quillard@gmail.com>
2021-12-05 17:10:17 +0100

    🥊 Select champions

--- src/Controller/MatchController.php
+++ src/Controller/MatchController.php
@@ -6,7 +6,11 @@ use App\Service\MatchService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class MatchController extends AbstractController
 {
@@ -15,8 +19,14 @@ class MatchController extends AbstractController
      */
     private $matchService;
 
-    public function __construct(MatchService $matchService) {
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    public function __construct(MatchService $matchService, SerializerInterface $serializer) {
         $this->matchService = $matchService;
+        $this->serializer = $serializer;
     }
 
     /**
@@ -25,8 +35,42 @@ class MatchController extends AbstractController
     public function index(Request $request): Response
     {
         // Lane, Player
+
         $params = json_decode($request->getContent(), true);
+        if(!isset($params['maxNumber']) || empty($params['maxNumber']) || !is_int($params['maxNumber'])) {
+            throw new HttpException(400, 'Missing max number');
+        }
+        if(!isset($params['username']) || empty($params['username']) || !is_string($params['username']) ) {
+            throw new HttpException(400, 'Missing username');
+        }
+
+        if(!isset($params['lane']) || empty($params['lane']) || !is_string($params['lane']) ) {
+            throw new HttpException(400, 'Missing lane');
+        }
+
+        if(!isset($params['ban']) || !is_array($params['ban']) ) {
+            throw new HttpException(400, 'Missing ban');
+        }
+
+        if(!isset($params['pick']['team']) || !is_array($params['pick']['team']) ) {
+            throw new HttpException(400, 'Missing pick team');
+        }
+
+        if(!isset($params['pick']['enemy']) || !is_array($params['pick']['enemy']) ) {
+            throw new HttpException(400, 'Missing pick enemy');
+        }
+
         $newMatch = $this->matchService->newMatch($params);
-        return $this->json($params);
+
+        $result = $this->serializer->serialize(
+            $newMatch,
+            'json',
+            [
+                AbstractNormalizer::ATTRIBUTES =>
+                    ['riotId', 'name', 'image', 'imageLoading', 'imageSplash', 'lane', 'type'
+                    ]
+            ]
+        );
+        return new JsonResponse($result, 200, [], true);
     }
 }
--- src/Service/MatchService.php
+++ src/Service/MatchService.php
@@ -2,14 +2,12 @@
 
 namespace App\Service;
 
+use App\Entity\BestMatchComposition;
 use App\Entity\Champion;
 use App\Entity\Composition;
 use App\Entity\Lane;
-use App\Entity\Match;
 use App\Entity\Player;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MatchService
 {
@@ -18,29 +16,20 @@ class MatchService
      */
     private $entityManager;
 
-    /**
-     * @var HttpClientInterface
-     */
-    private $httpClient;
-
-    /**
-     * @var ParameterBagInterface
-     */
-    private $parameterBag;
-
     public function __construct(
-        EntityManagerInterface $entityManager,
-        HttpClientInterface $httpClient,
-        ParameterBagInterface $parameterBag
+        EntityManagerInterface $entityManager
     )
     {
         $this->entityManager = $entityManager;
-        $this->httpClient = $httpClient;
-        $this->parameterBag = $parameterBag;
     }
 
-    public function newMatch(array $params) {
-        $championsNumber = $params['number'];
+    /**
+     * @param array $params
+     * @return array
+     */
+    public function newMatch(array $params): array
+    {
+        $championsNumber = $params['maxNumber'];
         $playerParams = $params['username'];
         $laneParams = $params['lane'];
         $pickParams = $params['pick'];
@@ -51,23 +40,34 @@ class MatchService
         $lane = $this->entityManager->getRepository(Lane::class)->findOneByName($laneParams);
         $favoriteRiotId = $this->getFavoriteRiotId($player);
 
+        $selectedChampions = $this->entityManager->getRepository(Champion::class)->getChampionsForIdsByLane($favoriteRiotId, $lane->getId());
+        $bestChampions = $this->entityManager->getRepository(Champion::class)->getChampionsByLane($lane->getId());
         $banChampions = $this->entityManager->getRepository(Champion::class)->getChampionsForids($banParams);
         $pickTeam = $this->entityManager->getRepository(Champion::class)->getChampionsForIds($pickFromTeamRiotId);
         $pickEnemy= $this->entityManager->getRepository(Champion::class)->getChampionsForIds($pickFromEnemyRiotId);
 
-        if(empty($pickFromEnemyRiotId)) {
-            $selectedChampions = $this->entityManager->getRepository(Champion::class)->getChampionsForIdsByLane($favoriteRiotId, $lane->getId());
-            $bestChampions = $this->entityManager->getRepository(Champion::class)->getChampionsByLane($lane->getId());
+        $selectedChampions = $this->removeBanAndPick($selectedChampions, $pickEnemy, $pickTeam, $banChampions);
+        $bestChampions = $this->removeBanAndPick($bestChampions, $pickEnemy, $pickTeam, $banChampions);
+
+        if(empty($pickFromEnemyRiotId) || count($pickFromEnemyRiotId) === 0) {
             $championsIdsCompositionWinRate = $this->getCompositionWinRateByChampions($bestChampions, $pickTeam, $banChampions);
             $championsCompositions = $this->removeEmptyAndBan($championsIdsCompositionWinRate, $banParams);
             return $this->mergeArrayWithWinRate($selectedChampions, $bestChampions, $championsCompositions, $championsNumber);
         }
         else {
-            $enemyPickBestCompo = $this->getCompositionEnemy($pickEnemy, $pickTeam, $banChampions);
-            $getBestMatchCompo = $this->getBestMatchLossesComposition($enemyPickBestCompo, $pickTeam);
+            $enemyPickBestComposition = $this->getCompositionEnemy($pickEnemy, $pickTeam, $banChampions);
+            $getTeamWinComposition = $this->getBestMatchLossesComposition($enemyPickBestComposition, $pickTeam);
+            $compareHashTeam = $this->compareHash($getTeamWinComposition, $pickTeam, $pickEnemy, $banChampions);
+            return $this->mergeArrayWithWinRate($selectedChampions, $bestChampions, $compareHashTeam, $championsNumber);
         }
     }
 
+    /**
+     * @param array $champions
+     * @param array $pick
+     * @param array $ban
+     * @return array
+     */
     public function getCompositionWinRateByChampions(array $champions, array $pick, array $ban): array
     {
         $pickHash = $this->createHash($pick);
@@ -77,16 +77,20 @@ class MatchService
 
             $hash = sprintf("%s#%s#%s",'%', $champion->getRiotId(), '%');
             $selectedComposition = $this->entityManager->getRepository(Composition::class)->getBestCompositionChampions($hash, $pickHash, $banHash, 1);
-            $compositionByChampion[] = [
-                "champion" => $champion,
-                "composition" => $selectedComposition
-            ];
+            $compositionByChampion[] = $selectedComposition;
         }
 
         return $compositionByChampion;
     }
 
-    public function getCompositionEnemy(array $enemyPick, array $teamPick, array $ban) {
+    /**
+     * @param array $enemyPick
+     * @param array $teamPick
+     * @param array $ban
+     * @return array
+     */
+    public function getCompositionEnemy(array $enemyPick, array $teamPick, array $ban): array
+    {
         $teamHash = $this->createHash($teamPick);
         $banHash = $this->createHash($ban);
         $cantPickHash = array_merge($teamHash, $banHash);
@@ -102,16 +106,41 @@ class MatchService
         return $compositionByChampion;
     }
 
-    public function getBestMatchLossesComposition(array $enemyCompositions, array $teamPick) {
+    /**
+     * @param array $enemyCompositions
+     * @param array $teamPick
+     * @return array
+     */
+    public function getBestMatchLossesComposition(array $enemyCompositions, array $teamPick): array
+    {
+        $compositionsWin = [];
         foreach ($enemyCompositions as $enemyComposition) {
-            $enemyCompositionsLosses = [];
             foreach ($enemyComposition['composition'] as $composition) {
-                dd($composition);
+                $bestMatch = $this->entityManager->getRepository(BestMatchComposition::class)->findByComposition($composition);
+                foreach ($bestMatch as $compositionHasWin) {
+                    if($compositionHasWin->getWin()) {
+                        continue;
+                    }
+                    $match = $this->entityManager->getRepository(BestMatchComposition::class)->findByBestMatch($compositionHasWin->getId());
+                    foreach ($match as $compositionTeam) {
+                        if(!$compositionTeam->getWin()) {
+                            continue;
+                        }
+                        $compositionWin = $this->entityManager->getRepository(Composition::class)->findOneById($compositionTeam->getComposition()->getId());
+                        $compositionsWin[] = $compositionWin;
+                    }
+                }
             }
         }
+        return $compositionsWin;
     }
 
-    public function createHash($champions) {
+    /**
+     * @param array $champions
+     * @return array
+     */
+    public function createHash(array $champions): array
+    {
         $hashArray = [];
         foreach ($champions as $champion) {
             $hash = sprintf('%s#%s#%s', '%', $champion->getRiotId(), '%');
@@ -120,7 +149,76 @@ class MatchService
         return $hashArray;
     }
 
-    public function getFavoriteRiotId(Player $player) {
+    /**
+     * @param array $champions
+     * @return string
+     */
+    public function createBanRegex(array $champions): string
+    {
+        $hash = "/^(?!.*(";
+        foreach ($champions as $index => $champion) {
+
+            if($index === 0) {
+                $hash = sprintf('%s#%s#', $hash, $champion->getRiotId());
+            } else {
+                $hash = sprintf('%s|#%s#', $hash, $champion->getRiotId());
+            }
+
+        }
+
+        return sprintf('%s)).*$/i', $hash);
+    }
+
+    /**
+     * @param array $champions
+     * @return string
+     */
+    public function createPickRegex(array $champions): string
+    {
+
+        $hash = "/";
+        foreach ($champions as $index => $champion) {
+            $hash = sprintf('%s(?=.*#%s#)', $hash, $champion->getRiotId());
+        }
+
+        return sprintf('%s/i', $hash);
+    }
+
+    /**
+     * @param array $compositions
+     * @param array $pick
+     * @param array $pickEnemy
+     * @param array $ban
+     * @return array
+     */
+    public function compareHash(array $compositions, array $pick, array $pickEnemy, array $ban): array
+    {
+        $validComposition = [];
+        $cantPick = array_merge($ban, $pickEnemy);
+        $checkBanHash = $this->createBanRegex($cantPick);
+        $checkPickHash = $this->createPickRegex($pick);
+        foreach ($compositions as $composition) {
+
+            $hash = $composition->getHash();
+            if(!preg_match($checkBanHash, $hash)) {
+                continue;
+            }
+
+            if(!preg_match($checkPickHash, $hash)) {
+                continue;
+            }
+
+            $validComposition[] = $composition;
+        }
+        return $validComposition;
+    }
+
+    /**
+     * @param Player $player
+     * @return array
+     */
+    public function getFavoriteRiotId(Player $player): array
+    {
         $favoriteIds = [];
         foreach($player->getFavorite() as $favorite) {
             $favorite = $favorite->getRiotId();
@@ -130,7 +228,13 @@ class MatchService
         return $favoriteIds;
     }
 
-    public function removeEmptyAndBan(array $array, array $ban) {
+    /**
+     * @param array $array
+     * @param array $ban
+     * @return array
+     */
+    public function removeEmptyAndBan(array $array, array $ban): array
+    {
         $returnArray = [];
         foreach ($array as $key)
         {
@@ -148,17 +252,105 @@ class MatchService
         return $returnArray;
     }
 
-    public function mergeArrayWithWinRate(array $favorites, array $bestChampions, array $bestCompositions, int $limit) {
-        dd($bestCompositions);
-        foreach ($bestCompositions as $champion) {
+    /**
+     * @param array $champions
+     * @param array $enemies
+     * @param array $picks
+     * @param array $bans
+     * @return array
+     */
+    public function removeBanAndPick(array $champions, array $enemies, array $picks, array $bans): array
+    {
+        foreach ($champions as $index => $champion) {
+            foreach ($enemies as $enemy) {
+                if($enemy->getId() !== $champion->getId()) {
+                    continue;
+                }
+                unset($champions[$index]);
+            }
 
+            foreach ($picks as $pick) {
+                if($pick->getId() !== $champion->getId()) {
+                    continue;
+                }
+                unset($champions[$index]);
+            }
+
+            foreach ($bans as $ban) {
+                if($ban->getId() !== $champion->getId()) {
+                    continue;
+                }
+                unset($champions[$index]);
+            }
         }
-        //$merged = array_merge($array1, $array2);
 
-        //usort($merged, [$this, 'sortArrayByKey']);
-        //return array_slice($merged, 0, 5);
+        return $champions;
     }
 
+    /**
+     * @param array $favorites
+     * @param array $bestChampions
+     * @param array $bestCompositions
+     * @param int $limit
+     * @return array
+     */
+    public function mergeArrayWithWinRate(array $favorites, array $bestChampions, array $bestCompositions, int $limit): array
+    {
+        $compositionByChampion = [];
+        foreach ($bestCompositions as $composition) {
+
+            foreach ($favorites as $index => $favorite) {
+                $hash = $this->createPickRegex([$favorite]);
+
+                if(!preg_match($hash, $composition->getHash())) {
+                    continue;
+                }
+
+                /*$compositionByChampion[] = [
+                    "champions" => $favorite,
+                    "composition" => $composition
+                ];*/
+
+                $compositionByChampion[] = $favorite;
+
+                unset($favorites[$index]);
+            }
+
+            foreach ($bestChampions as $index => $bestChampion) {
+                $hash = $this->createPickRegex([$bestChampion]);
+
+                if(!preg_match($hash, $composition->getHash())) {
+                    continue;
+                }
+
+                /*$compositionByChampion[] = [
+                    "champion" => $bestChampion,
+                    "composition" => $composition
+                ];*/
+                $compositionByChampion[] = $bestChampion;
+
+                unset($bestChampions[$index]);
+            }
+        }
+
+        if(count($compositionByChampion) !== $limit) {
+            $diff = $limit - count($compositionByChampion);
+            $merged = array_merge($favorites, $bestChampions);
+            usort($merged, [$this, 'sortArrayByKey']);
+            $defaultChampions = array_slice($merged, 0, $diff);
+            foreach ($defaultChampions as $defaultChampion) {
+                $compositionByChampion[] = $defaultChampion;
+            }
+        }
+
+        return $compositionByChampion;
+    }
+
+    /**
+     * @param $a
+     * @param $b
+     * @return int
+     */
     public function sortArrayByKey($a, $b): int
     {
         $al = $a->getWinRate();
